d0c0721abfac481d23ca8635031ca65f83822918
Ana Khomasuridze <a.khomasuridze@intelexpress.ge>
2022-08-07 14:04:38 +0400

    Fixed MailReceiver(Config files)

--- ExcelConverter/Program.cs
+++ ExcelConverter/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MailReceiver;
+
 Console.WriteLine("Hello, World!");
+
+ReceiveMail receiveMail = new ReceiveMail();
+
+receiveMail.SaveMailAttachments();

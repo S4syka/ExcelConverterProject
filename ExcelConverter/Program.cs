bea4afe6a6dfad09b283eb5fc97370cf9714f2af
Ana Khomasuridze <a.khomasuridze@intelexpress.ge>
2022-08-09 15:48:58 +0400

    Added GetDayOneDTOs in ExcelReader

--- ExcelConverter/Program.cs
+++ ExcelConverter/Program.cs
@@ -1,13 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MailReceiver;
 using MailSender;
+using ExcelReader;
 
 Console.WriteLine("Hello, World!");
 
 ReceiveMail receiveMail = new ReceiveMail();
 
 receiveMail.SaveMailAttachments();
-
+/*
 SendMail sendMail = new SendMail();
 
 sendMail.SendMailAttachments("e.khomasuridzemails@yahoo.com","test123");
+*/
+ReadDayOne readDayOne = new(@"C:\Users\Rabbitt\Downloads\20220809_Portfolio_BG-GEORGIAN BUILDING GROUP_1.xlsx");
+
+var xd = readDayOne.Items();
+
+Console.WriteLine("123");
--- ExcelReader/ReadDayOne.cs
+++ ExcelReader/ReadDayOne.cs
@@ -1,9 +1,68 @@
-﻿using ExcelConverter.Domain.DTO;
+﻿using System.IO;
+using System.Configuration;
+using ExcelConverter.Domain.DTO;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelReader
 {
     public class ReadDayOne
     {
+        _Application excel = new _Excel.Application();
+        Workbook workBook;
+        Worksheet workSheet;
 
+        private string Path { get; set; }
+
+        public ReadDayOne(string path)
+        {
+            this.Path = path;
+            workBook = excel.Workbooks.Open(path);
+            workSheet = workBook.Worksheets[2];
+        }
+
+        private double ReadCell(int i, int j)
+        {
+            if (workSheet.Cells[i][j].Value != null)
+            {
+                return workSheet.Cells[i][j].Value;
+            }
+            else return 0;
+        }
+
+        private IEnumerable<OneDayEarly> GetDayOneDTO()
+        {
+            for (int i = 1; i < 25; i++)
+            {
+                yield return new OneDayEarly()
+                {
+                    Hour = i,
+                    PredictionTwoDays = ReadCell(2, i + 6),
+                    UnbalanceTwoDays = ReadCell(3, i + 6),
+                    ContractSum = ReadCell(4, i + 6),
+                    UnbalanceOneDay = ReadCell(5, i + 6),
+                    PredictionOneDay = ReadCell(6, i + 6),
+                    VolumeOneday = ReadCell(7, i + 6),
+                    Price = Convert.ToInt32(ReadCell(8, i + 6)),
+                    IncomePrediction = Convert.ToInt32(ReadCell(9, i + 6))
+                };
+            }
+        }
+
+        public IEnumerable<IEnumerable<OneDayEarly>> GetDayOneDTOs()
+        {
+            string directoryPath = ConfigurationManager.AppSettings["SaveFileAddress"];
+            directoryPath = directoryPath + @"\" + DateTime.Now.ToString("dd-MM-yyyy");
+            var contents = Directory.GetFiles(directoryPath,"*.xml");
+            foreach (var items in contents)
+            {
+                workBook = excel.Workbooks.Open(path);
+                workSheet = workBook.Worksheets[2];
+
+                yield return GetDayOneDTO();
+
+                workBook.Close();
+            }
+        }
     }
 }
